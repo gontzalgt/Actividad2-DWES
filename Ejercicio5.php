3c29449906a845720eecd379d3e08175d0b6614a
gontzalgt <78103594+gontzalgt@users.noreply.github.com>
2022-09-26 09:49:25 +0200

    Add files via upload

---
+++ Ejercicio5.php
@@ -0,0 +1,10 @@
+<?php
+$num1 = 10;
+$num2 = 30;
+
+$resultado = $num1 + $num2;
+echo "La suma es: " ,$resultado;
+echo "<br>";
+echo "Enlace: https://github.com/gontzalgt/Actividad2-DWES/blob/main/Ejercicio5.php";
+
+?>
