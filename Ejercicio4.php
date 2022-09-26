e64e8372149be535ee65a529c1c507552a9a05c2
gontzalgt <78103594+gontzalgt@users.noreply.github.com>
2022-09-26 09:47:31 +0200

    Add files via upload

--- Ejercicio4.php
+++ Ejercicio4.php
@@ -3,4 +3,6 @@ $ciclo="DAW";
 $modulo="DWES";
 print "<p>";
 printf("%s es un módulo de %d curso de %s", "<b>$modulo</b>", 2, "<b>$ciclo</b>");
+print("<br>");
+printf("Enlace: https://github.com/gontzalgt/Actividad2-DWES/blob/main/Ejercicio4.php");
 print "</p>";
